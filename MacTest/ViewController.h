3e7e895069a202b19dd3a47cd16d97a563721126
Mr Song <song@wwdeimac.local>
2016-12-16 16:18:01 +0800

    Initial Commit

---
+++ MacTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacTest
+//
+//  Created by MrSong on 12/16/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ MacTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MacTest
+//
+//  Created by MrSong on 12/16/16.
+//  Copyright © 2016 song. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
